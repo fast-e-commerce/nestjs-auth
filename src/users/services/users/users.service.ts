220ac7ec4498fb848ee551b44b847a32c60abe09
NelsonFrank <nelsonfr78@outlook.com>
2024-03-08 23:28:31 +0300

    feat: :sparkles: add findOne fn in users service

--- src/users/services/users/users.service.ts
+++ src/users/services/users/users.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
 
+export type IUser = {
+  userId: number;
+  username: string;
+  password: string;
+};
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly users = [
+    {
+      userId: 1,
+      username: 'john',
+      password: 'changesoon',
+    },
+    {
+      userId: 2,
+      username: 'alex',
+      password: 'changelater',
+    },
+  ];
+
+  async findOne(username: string): Promise<IUser | undefined> {
+    return this.users.find((user) => user.username === username);
+  }
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './services/users/users.service';
 
 @Module({
-  providers: [UsersService]
+  providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
