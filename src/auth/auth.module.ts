de6e5b6d1c5d26688d270aa47b75ffde4ddfca32
NelsonFrank <nelsonfr78@outlook.com>
2024-03-08 23:11:21 +0300

    fix: :bug: remove duplicate in auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
 import { AuthController } from './controllers/auth/auth.controller';
 import { AuthService } from './services/auth/auth.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
