f17aa0e4dbda4e726c726eba242a06c0d1276593
NelsonFrank <nelsonfr78@outlook.com>
2024-03-09 00:03:00 +0300

    feat: :sparkles: add signIn controller in auth controller

--- src/auth/controllers/auth/auth.controller.ts
+++ src/auth/controllers/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from 'src/auth/services/auth/auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('login')
+  signIn(@Body() signInDto: Record<string, any>) {
+    return this.authService.signIn(signInDto.username, signInDto.password);
+  }
+}
