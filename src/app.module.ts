d52c111fa20cf9509427d377efa0986bb69f28b3
NelsonFrank <nelsonfr78@outlook.com>
2024-03-08 23:12:59 +0300

    feat: :sparkles: add user module and service boilerplate code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UsersModule],
   controllers: [],
   providers: [],
 })
