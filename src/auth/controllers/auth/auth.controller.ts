1fb4045ce1a7a91fda80c3f67fe8f463e940b1d5
NelsonFrank <nelsonfr78@outlook.com>
2024-03-14 11:06:07 +0300

    feat: :sparkles: add a isPublic custom decorator that flag which routes should be public

--- src/auth/controllers/auth/auth.controller.ts
+++ src/auth/controllers/auth/auth.controller.ts
@@ -1,12 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Request, Get } from '@nestjs/common';
 import { AuthService } from 'src/auth/services/auth/auth.service';
+import { Public } from 'src/auth/decorators/auth/auth.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @Post('login')
   signIn(@Body() signInDto: Record<string, any>) {
     return this.authService.signIn(signInDto.username, signInDto.password);
+  }
+
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
